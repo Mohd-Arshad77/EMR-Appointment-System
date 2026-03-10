a10ae7b4934f2faf65097078432de72d294ce306
arsshuz <arshadimb786@gmail.com>
2026-03-10 13:05:51 +0530

    updated backend render API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5005/api',
+  baseURL: 'https://emr-backend-iorx.onrender.com/api',
   withCredentials: true
 });
 
